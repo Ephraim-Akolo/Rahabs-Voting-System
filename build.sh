fc3fe05a1e0a2fda162a475e7d58ba0ce4e0639a
Ephraim <ephraimakolo2017@gmail.com>
2023-11-03 09:27:05 +0100

    removed the election button

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; import os; User = get_user_model(); True if User.objects.filter(email=f'{os.getenv("DJANGO_SUPERUSER_USERNAME")}') else User.objects.create_superuser(f'{os.getenv("DJANGO_SUPERUSER_USERNAME")}', f'{os.getenv("DJANGO_SUPERUSER_EMAIL")}', f'{os.getenv("DJANGO_SUPERUSER_PASSWORD")}')" | python manage.py shell
