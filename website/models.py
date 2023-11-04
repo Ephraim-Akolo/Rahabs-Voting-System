08310b8452240bac3acbe627ce6c1696c741e360
Ephraim <ephraimakolo2017@gmail.com>
2023-11-04 21:13:49 +0100

    added models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class state(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class LocalGorvernment(models.Model):
+    state = models.ForeignKey(state, on_delete=models.CASCADE)
+    name = models.CharField(max_length=10)
+
+
+class User(AbstractUser):
+
+    vin = models.CharField(max_length=20, null=True, unique=True)
+    lga = models.ForeignKey(LocalGorvernment, null=True, on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.username
+    
+
+class PoliticalParty(models.Model):
+    name = models.CharField(max_length=10)
+    
+
+class Election(models.Model):
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    party = models.ForeignKey(PoliticalParty, on_delete=models.CASCADE)
+    lga = models.ForeignKey(LocalGorvernment, on_delete=models.CASCADE)
+
