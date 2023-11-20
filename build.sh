98412901a8d88f82c2b8900886aee585696f4ae3
Ephraim <ephraimakolo2017@gmail.com>
2023-11-20 22:55:14 +0100

    build update

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-apt update -y && apt install libgl1 -y
+apt update -y && apt install libgl1 -y && apt install curl -y
 pip install --upgrade pip
-apt install curl -y
 curl -o . https://github.com/sachadee/Dlib/blob/main/dlib-19.22.99-cp37-cp37m-win_amd64.whl
 pip install ./dlib-19.22.99-cp37-cp37m-win_amd64.whl
+rm ./dlib-19.22.99-cp37-cp37m-win_amd64.whl
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
